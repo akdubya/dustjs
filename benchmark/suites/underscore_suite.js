6b2aeb7a0233f6913dd8278178b26d9163a1b209
Kristofer Joseph <kristoferjoseph@gmail.com>
2012-07-05 22:18:52 -0700

    Adding underscore to the shootout page

---
+++ benchmark/suites/underscore_suite.js
@@ -0,0 +1,113 @@
+(function(exports){
+
+var benches = {
+
+  string: {
+    source:  "Hello World!",
+    context: {}
+  },
+
+  replace: {
+    source:  "Hello <%= name %>! You have <%= count %> new messages.",
+    context: { name: "Mick", count: 30 }
+  },
+
+  array: {
+    source:   "<% _.each(names, function(name) { %> <%= name %> <% }); %>",
+    context:  { names: [{name: "Moe"}, {name: "Larry"}, {name: "Curly"}, {name: "Shemp"}] }
+  },
+
+  object: {
+    source:   "<%= person.name %> <%= person.age %>",
+    context:  { person: { name: "Larry", age: 45 } }
+  },
+
+  partial: {
+    source:   "<% _.each(peeps, function(peep) { %> <%= partials.myPartialTemplate(peep) %> <% }); %>",
+    context:  { peeps: [{name: "Moe", count: 15}, {name: "Larry", count: 5}, {name: "Curly", count: 1}] },
+    partials: { myPartialTemplate: "Hello <%= name %>! You have <%= count %> new messages." }
+  },
+
+  recursion: {
+    source:   "<%= name %> <% _.each(kids, function(kid) { %> <%= recursion %> <% }); %>",
+    context:  {
+                name: '1',
+                kids: [
+                  {
+                    name: '1.1',
+                    kids: [
+                      {name: '1.1.1'}
+                    ]
+                  }
+                ]
+              },
+    partials: { recursion: "<%= name %> <% _.each(kids, function(kid) { %> <%= recursion %> <% }); %>" }
+  },
+
+  filter: {
+    source:   "foo <%= filter(bar) %>",
+    context:  {
+                filter: function(ctx) {
+                  return ctx.toUpperCase();
+                },
+                bar: "bar"
+              }
+  },
+
+  complex: {
+    source:  "<h1><%= header %></h1>\n" +
+             "<% if(hasItems) { %>" +
+             "  <% _.each(items, function(item) { %>\n" +
+             "    <ul>\n" +
+             "        <% if(item.current) { %>\n" +
+             "          <li><strong> <%- item.name %> </strong></li>\n" +
+             "        <% } else { %>\n" +
+             "          <li><a href=\" <%- item.url %> \"> <%- item.name %> </a></li>\n" +
+             "        <% } %>\n" +
+             "    </ul>\n" +
+             "  <% }); %>" +
+             "<% } else { %>\n" +
+             "  <p>The list is empty.</p>\n" +
+             "<% } %>",
+    context: {
+               header: function() {
+                 return "Colors";
+               },
+               items: [
+                 {name: "red", current: true, url: "#Red"},
+                 {name: "green", current: false, url: "#Green"},
+                 {name: "blue", current: false, url: "#Blue"}
+               ],
+               hasItems: function(ctx, fn) {
+                 if (ctx.items.length) {
+                   return fn(ctx);
+                 }
+               }
+             }
+  }
+
+};
+
+exports.underscoreBench = function(suite, name, id) {
+  var bench = benches[name],
+      fn = _.template(bench.source),
+      ctx = bench.context,
+      partials = {};
+
+  if (bench.partials) {
+    for (var key in bench.partials) {
+      partials[key] = _.template(bench.partials[key]);
+    }
+  }
+
+  ctx.partials = partials;
+
+  suite.bench(id || name, function(next) {
+    fn(ctx);
+    next();
+  });
+};
+
+exports.underscoreBench.benches = benches;
+
+})(typeof exports !== "undefined" ? exports : window);
